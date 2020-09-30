aa292bd3e01f26ff92fdf68c3197e2fdd53ad019
ginrut <gintauter@yahoo.com>
2020-09-30 12:25:11 +0300

    math

---
+++ js/devide.js
@@ -0,0 +1,5 @@
+function devide(a, b) {
+    return a / b;
+}
+
+export default devide;
---
+++ js/functions.js
@@ -0,0 +1,8 @@
+import sum from './sum.js';
+import devide from './devide.js';
+
+
+
+console.log(sum(2,8))
+
+console.log(devide(9,3))
